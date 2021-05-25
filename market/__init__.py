52c0e47115286fc6ed8310692ddfe2294dbb9e41
Rafael Montes <rafaelmontes1997@gmail.com>
2021-05-25 10:51:32 -0400

    Add Register functionality

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '3696f3fc9a45dbd2cfaf77be'
 db = SQLAlchemy(app)
 
 from market import routes
